6cb10244860e2f1f5378de59b37a163fb1efee2a
Tony Lorenzo <tlorenzo@doubleprime.com>
2015-10-08 12:36:01 -0400

    Adding mutators for pagination mixin.

--- mixin/paginate.js
+++ mixin/paginate.js
@@ -4,7 +4,9 @@ ThinMint.Mixin.Paginate = function() {
 
   _super.init = ThinMint.Util.callback( this.init );
   _super.getPage = ThinMint.Util.callback( this.getPage );
+  _super.setPage = ThinMint.Util.callback( this.setPage );
   _super.getPages = ThinMint.Util.callback( this.getPages );
+  _super.setPages = ThinMint.Util.callback( this.setPages );
   _super.pageNext = ThinMint.Util.callback( this.pageNext );
   _super.pagePrevious = ThinMint.Util.callback( this.pagePrevious );
   _super.pageTo = ThinMint.Util.callback( this.pageTo );
@@ -20,9 +22,21 @@ ThinMint.Mixin.Paginate = function() {
   this.getPage = function() {
     return this.page;
   };
+
+  this.setPage = function(page) {
+    this.page = Number(page);
+    return this;
+  };
+
   this.getPages = function() {
     return this.pages;
   };
+
+  this.setPages = function(pages) {
+    this.pages = Number(pages);
+    return this;
+  };
+
   this.pageNext = function() {
     if(this.page >= this.pages) {
       console.warn('ThinMint.Mixin.Paginate', 'Already at the max page.');
@@ -32,6 +46,7 @@ ThinMint.Mixin.Paginate = function() {
     ++this.page;
     return this.onPage();
   };
+
   this.pagePrevious = function() {
     if(this.page <= this.pageFirst) {
       console.warn('ThinMint.Mixin.Paginate', 'Already at the first page.');
@@ -41,6 +56,7 @@ ThinMint.Mixin.Paginate = function() {
     --this.page;
     return this.onPage();
   };
+
   this.pageTo = function(page) {
     page = parseInt(page);
 
@@ -53,6 +69,7 @@ ThinMint.Mixin.Paginate = function() {
     this.page = page;
     return this.onPage();
   };
+
   this.onPage = function() {
     // Each panel will imlement onPage, this is how it
     // will handle a page change.
@@ -60,5 +77,4 @@ ThinMint.Mixin.Paginate = function() {
   };
 
   return this;
-
 };
